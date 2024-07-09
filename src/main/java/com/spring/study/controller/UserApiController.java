d9e6ea699b3887b7d103500e9c6aae99c738a0a5
CloakingGhost <project0858@gmail.com>
2024-07-09 20:00:56 +0900

    드가자

--- src/main/java/com/spring/study/controller/UserApiController.java
+++ src/main/java/com/spring/study/controller/UserApiController.java
@@ -1,16 +1,16 @@
 package com.spring.study.controller;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.study.dto.CustomUser;
 
 @Controller
 @RequestMapping("/api")
@@ -30,14 +30,14 @@ public class UserApiController {
 		return str;
 	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@RequestMapping(value = "/login/fetch", method = RequestMethod.POST)
 	@ResponseBody
-	public String validateInfo(Map<String,Object> map) {
+	public String validateInfo(@RequestBody CustomUser user) {
 		ObjectMapper mapper = new ObjectMapper();
 		
 		String jsonStr = null;
 		try {
-			jsonStr = mapper.writeValueAsString(map);
+			jsonStr = mapper.writeValueAsString(user);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/com/spring/study/dto/CustomUser.java
@@ -0,0 +1,41 @@
+package com.spring.study.dto;
+
+public class CustomUser {
+	private String name;
+	private String password;
+	private int age;
+
+	public CustomUser() {
+	}
+
+//	public CustomUser(String name, String password, int age) {
+//		this.name = name;
+//		this.password = password;
+//		this.age = age;
+//	}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
